72d255448f04d40030cc4332dca3b6c4dbdecb79
ToxaRat <toxarat@gmail.com>
2024-02-21 11:15:25 +0200

    test add

---
+++ test.py
@@ -0,0 +1,45 @@
+class Contacts:
+    current_id = 1
+
+    def __init__(self):
+        self.contacts = []
+
+    def list_contacts(self):
+        return (self.contacts)
+   
+    def add_contacts(self, name, phone, email, favorite):
+        self.contacts.append(
+            {
+                "id": Contacts.current_id,
+                "name": name,
+                "phone": phone,
+                "email": email,
+                "favorite": favorite,
+            }
+        )
+        Contacts.current_id += 1
+
+    def get_contact_by_id(self, id):
+        result = list(filter(lambda contact: contact.get("id") == id, self.contacts))
+        return result[0] if len(result) > 0 else None
+    
+    def remove_contacts(self, id):
+        for el in self.contacts:
+            if el["id"] == id:
+                print("ID Find: " , id)
+                self.contacts.remove(el)
+                return
+ 
+def main():
+    c = Contacts()
+    c.add_contacts("Wylie Pope", "(692) 802-2949", "est@utquamvel.net", True)
+    c.add_contacts("Cyrus Jackson", "(501) 472-5218", "nibh@semsempererat.com", False)
+    print(c.list_contacts())
+    print(c.get_contact_by_id(1))
+    c.remove_contacts(1)
+    print(c.list_contacts())
+
+    
+
+if __name__ == "__main__":
+    main()
